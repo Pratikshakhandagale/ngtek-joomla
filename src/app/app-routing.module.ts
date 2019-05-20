6c1830a2b420876e7b95af6969539603f95f1fd8
Pratiksha Khandagale <pratiksha_k@techjoomla.com>
2019-05-20 14:54:45 +0530

    Task #144836 feat:Joomla article and user login Integration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ArticleComponent } from './article/article.component';
+import { ListComponent } from './list/list.component';
+import { ArticleEditComponent } from './article-edit/article-edit.component';
+import { DetailsComponent } from './details/details.component';
+import {UserComponent } from './user/user.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UserComponent },
+  { path: 'articles', component: ArticleComponent },
+  { path: 'article/:id', component: ArticleEditComponent },
+  { path: 'details/:id', component: DetailsComponent },
+  { path: 'article', component: ArticleEditComponent },
+  { path: 'blogs', component: ArticleComponent},
+  { path: 'list', component: ListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
+import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
-
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-
-//Componenets
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http'; 
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { ListComponent } from './list/list.component';
+import { ArticleEditComponent } from './article-edit/article-edit.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ContentLibModule } from 'projects/content-lib/src/lib/content-lib.module';
+import { UserLoginModule } from 'projects/user-login/src/lib/user-login.module';
 
-const appRoutes: Routes = [
+import { FormsModule } from '@angular/forms';
+import { AngularEditorModule } from '@kolkov/angular-editor';
+import { DetailsComponent } from './details/details.component';
+import { UserComponent } from './user/user.component';
+//  googleClientId : '1005635293950-29g89s87fm0dvj3540rsufta0oguts1l.apps.googleusercontent.com',
 
-];
-
+const config = {
+  googleClientId : '312478341535-t542no2kv0ffdskdshkreas4drf1ihc1.apps.googleusercontent.com',
+  fbAppId : '2037079629930934',
+  baseUrl: 'https://smart-citizen.tekdi.net'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent,
+    HeaderComponent,
+    ListComponent,
+    ArticleEditComponent,
+    DetailsComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    ),
-    MDBBootstrapModule.forRoot()
-
+    HttpClientModule,
+    ReactiveFormsModule,
+    ContentLibModule.forRoot(environment),
+    FormsModule,
+    AngularEditorModule,
+    UserLoginModule.forRoot(config)
   ],
-  providers: [],
-  bootstrap: [AppComponent],
-  schemas: [ NO_ERRORS_SCHEMA ]
-
+  schemas: [ NO_ERRORS_SCHEMA ],
+  providers: [HttpClientModule],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
